4f73aa024fa66fd7127678df2d254b724363bc20
Jeevan Muthu <75674374+Jeevan2305@users.noreply.github.com>
2025-01-27 21:59:32 +0530

    Add day2.py

---
+++ Python/Day-02/day2.py
@@ -0,0 +1,9 @@
+name = "Jeevan"
+age = 22
+height = 176.00
+is_student = False
+email = None
+siblings = ['brother','sister']
+address = {'city':'Chennai', 'state':'Tamil Nadu', 'country':'India'}
+
+print(name, age, height, is_student, email, siblings, address)
