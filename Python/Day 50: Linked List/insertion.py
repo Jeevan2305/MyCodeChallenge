fd80bb006cea0e92b075fabd00ce31ae7d723438
Jeevan Muthu <75674374+Jeevan2305@users.noreply.github.com>
2025-03-26 21:38:11 +0530

    Update insertion.py

--- Python/Day 50: Linked List/insertion.py
+++ Python/Day 50: Linked List/insertion.py
@@ -1,1 +1,12 @@
 # Linked List - Insertion
+class Node:
+    def __init__(self, data = None, next = None):
+        self.data = data
+        self.next = next
+        
+class LinkedList:
+    def __init(self):
+        self.head = None
+
+if __name__ == "__main__":
+    pass
